a3f36753e229308e52c616b22b8f608631681005
Radwan <radwanskipawel48@gmail.com>
2022-12-26 19:49:42 +0100

    This is work !

--- open.py
+++ open.py
@@ -1,19 +1,13 @@
 import sqlite3
-import webbrowser
-
-db = sqlite3.connect('run_webbrowser2_0.db')
+db= sqlite3.connect('run_webbrowser2_0.db')
 cursor = db.cursor()
-x=str(input("Podaj liczbę : "))
-def openbrowser():
 
-    query = f'SELECT Link FROM score where id =={x}'
-    cursor.execute(query)
-    rows= cursor.fetchall()
-    for r in rows:
-        webbrowser.open(str(r[0]))
-    # db.commit()
-    # db.close()
+query = "SELECT Link FROM score WHERE id = 2 "
+cursor.execute(query)
+rows = cursor.fetchone()
 
+for r in rows:
+    print(r)
 
-
-openbrowser()
+db.commit()
+db.close()
--- run_webbrowser2.py
+++ run_webbrowser2.py
@@ -1,35 +1,33 @@
-import sqlite3
-# import pyttsx3
-import webbrowser
 import tkinter as t
+import webbrowser
+import sqlite3
 from show_records import show_records
 
-# engine = pyttsx3.init()
-window= t.Tk()
-window.title("RunWebBrowser 2.0")
-window.geometry("500x300")
-
-show_records()
-
 db = sqlite3.connect('run_webbrowser2_0.db')
-cursor = db.cursor()
+cursor= db.cursor()
 
-entry = str(t.Entry(window, width=10,justify='center'))
+window = t.Tk()
+window.title('Run webbrowser 2')
+window.geometry("500x300")
+show_records()
+entry = t.Entry(window, width=10, justify='center')
+entry.pack()
+
 def open():
-    global entry
-    entry==entry.pack()
-    query = f'SELECT Link FROM score WHERE id=={entry}'
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for r in rows:
-        webbrowser.open(str(r[0]))
+    query = f"SELECT Link FROM score WHERE id == ?"
+    link_id = entry.get()
+    cursor.execute(query,(link_id))
+    link = cursor.fetchone()
+
+    if link is None:
+        t.messagebox.showinfo("Błąd", "Nie znaleziono linku o podanym identyfikatorze")
+    else:
+        webbrowser.open(link[0])
 
 
 
-
-t.Button(window,text="OK", command=open).pack()
+button = t.Button(window, text="OK", command=open).pack()
 
 window.mainloop()
-
-db.commit()
 db.close()
+

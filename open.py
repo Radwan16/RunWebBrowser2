e42268fef82a4db11afaa760c2c6d5a2575ce855
Radwan16 <32171764+Radwan16@users.noreply.github.com>
2023-01-08 21:02:20 +0100

    Update open.py
    delete file

--- open.py
+++ open.py
@@ -1,13 +1,1 @@
-import sqlite3
-db= sqlite3.connect('run_webbrowser2_0.db')
-cursor = db.cursor()
 
-query = "SELECT Link FROM score WHERE id = 2 "
-cursor.execute(query)
-rows = cursor.fetchone()
-
-for r in rows:
-    print(r)
-
-db.commit()
-db.close()
